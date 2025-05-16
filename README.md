8fa9da4dba492ef268020d130746fd0df6233f20
javie.rodriguez@bue.edu.ar <javiergallard91@gmail.com>
2025-05-15 22:35:36 -0300

    JavaScript V

--- README.md
+++ README.md
@@ -1,6 +1,246 @@
 # UTN2025_131 :telescope:
 Repositorio de la TUP UTN 2025, Comision 131
 
+## JavaScript
+```js
+////////////////////
+// JavaScript IV //
+
+// Introduccion a arrays y objetos. Metodos de strings y arrays
+// Arrays y objetos son estructuras de datos fundamentales
+
+/*
+Uso principal:
+    Array: Lista ordenada de elementos
+    Objeto: Coleccion de pares clave-valor
+
+Acceso a datos:
+    Array: Por indice -> 0
+    Objeto: Por clave
+
+Metodos:
+    Array: .push(), .pop(), .map(). .forEach()
+    Objeto: Metodos personalizados
+
+Iteracion:
+    Array: bucles, .forEach(), .map()
+    Objeto: for.. in, Object.keys(), Object.values()
+*/
+
+
+/////////////
+// Arrays //
+// Arrays para almacenar una lista ORDENADA de elementos
+let frutas = ["pera", "banana", "mandarina"];
+
+console.log(frutas[0]); // pera
+
+
+/////////////
+// Objetos//
+// Objetos para almacenar datos con propiedades CLAVE-VALOR
+let alumno = {
+    nombre: 'Camilo',
+    edad: 19,
+    ciudad: "Lanus",
+
+    agitar: function() {
+        console.log("Aguante Lanus");
+    }
+}
+
+// Notacion de punto
+console.log(alumno.nombre); //
+
+// Notacion de corchete
+console.log(alumno["ciudad"]); //
+
+alumno.agitar();
+
+// Agregar una propiedad
+alumno.pais = "Argentina";
+
+console.log(alumno);
+
+// Eliminar una propiedad
+delete alumno.edad;
+console.log(alumno);
+
+
+// Ejemplo completo con un Objeto
+let libro = {
+    titulo: "El Eternauta",
+    autor: "Héctor Germán Oesterheld",
+    anio: 1957,
+
+    detalles: function() {
+        return `${this.titulo} es un libro escrito por ${this.autor} en el año ${this.anio}.`;
+    }
+};
+
+console.log(libro.detalles());
+
+libro.editorial = "Hora Cero Semanal";
+
+console.log(libro);
+
+
+////////////////////////////////////////////////
+// Object wrappers - Envolvedores de objetos //
+
+// JavaScript otorga metodos a tipos de datos primitivos
+// Por eso se dice que en JavaScript son todo objetos (incluso los tipos de datos que no son objetos)
+
+// Metodos de Strings en JavaScript
+// https://drive.google.com/drive/u/1/folders/1QJtNkPRSPVN4S2WCuwoeNcxrZbZkPH0y
+// https://www.w3schools.com/js/js_string_methods.asp
+
+
+// Convertir a mayusculas o minusculas
+let texto1 = "JavaScript esta re bueno";
+console.log(texto1.toUpperCase()); // JAVASCRIPT ESTA RE BUENO
+console.log(texto1.toLowerCase()); // javascript esta re bueno
+
+
+// Reemplazamos valores
+let texto2 = "JavaScript es dificil";
+let nuevoTexto2 = texto2.replace("dificil", "facil");
+console.log(nuevoTexto2);
+
+
+// Extaer una parte del string sin alterar el string original
+let texto3 = "Aprendiendo JavaScript";
+let parte3 = texto3.slice(11, 22);
+console.log(parte3);
+
+
+// Dividir un string en un array de subcadenas
+let texto4 = "Manzana, Naranja, Pomelo";
+let frutas4 = texto4.split(", ");
+console.log(frutas4);
+
+let pruebaLoop = "Alejandro";
+for(let i = 0; i < pruebaLoop.length; i++) {
+    console.log(pruebaLoop[i]);
+}
+
+
+// Eliminamos los espacios en blanco, al principio y al final de un string (también teemos trimStart y trimEnd)
+let texto5 = "    JavaScript     ";
+console.log(texto5.trim());
+
+
+// Reemplazar un caracter por otro (recuerden que hay replace() y replaceAll())
+let texto6 = "Java    Script";
+console.log(texto6.replaceAll(" ", "")); // Con replaceAll reemplazamos TODOS
+
+
+// Devuelve el caracter en una posicion especifica
+let texto7= "Hola";
+console.log(texto7.charAt(0));
+
+
+// Verificamos si un string contiene un valor especifico
+let texto8 = "Aprendiendo JavaScript";
+console.log(texto8.includes("Java"));
+
+
+/*Ejercicio propuesto
+Crear una funcion que reciba una cadena de texto y
+    - La convierta a mayusculas
+    - Reemplace una palabra dentro del string
+    - Elimine los espacios en blanco sobrantes*/
+
+
+
+// Metodos de arrays en JavaScript
+// https://drive.google.com/drive/u/1/folders/1QJtNkPRSPVN4S2WCuwoeNcxrZbZkPH0y
+// https://www.w3schools.com/js/js_array_methods.asp
+
+// Un array es una estructura ORDENADA de datos que almacena varios valores en una sola variable
+// Pueden contener distintos tipos de datos: numeros, strings, booleanos, etc
+
+let numeros = [1, 2, 3, 4, 5];
+
+
+// Modificamos elementos de un array
+numeros[2] = 10;
+console.log(numeros);
+
+let verduras = ["Rucula", "Lechuga", "Albahaca"];
+
+
+// Añadimos uno o mas elementos al FINAL del array
+verduras.push("Tomate");
+console.log(verduras);
+
+
+// Eliminar el ultimo valor de un array
+let ultimaVerdura = verduras.pop();
+console.log(ultimaVerdura);
+console.log(verduras);
+
+
+// Eliminar el PRIMER valor de un array
+verduras.shift();
+console.log(verduras);
+
+
+// Añadimos uno o mas elementos al INICIO del array
+verduras.unshift("Ajo");
+console.log(verduras);
+
+
+// EXTRA: Otras estructuras de datos
+
+// Date 
+// Estructura para manejar fechas y horas
+const fecha = new Date("2023-12-25");
+console.log(fecha.getFullYear());
+
+
+// JSON (JavaScript Object Notation)
+// Estructura para representar datos en texto. Se convierte a/desde objetos con JSON.stringify() y JSON.parse()
+const persona = { nombre: "Ana", edad: 30 };
+
+const json = JSON.stringify(persona); // {"nombre":"Ana","edad":30}
+console.log(json);
+
+const obj = JSON.parse(json);
+console.log(obj); 
+
+
+// Map
+// Coleccion de pares clave-valor donde las claves puede ser de cualquier tipo
+const mapa = new Map()
+mapa.set("nombre", "Ana");
+mapa.set(123, "ID");
+mapa.set(true, "activo");
+
+console.log(mapa.get("nombre")); // Ana
+console.log(mapa.has(123)); //true
+
+
+// Set
+// Coleccion de valores unicos. NO permite valores duplicados
+const conjunto = new Set([1, 2, 2, 3]);
+conjunto.add(4);
+console.log(conjunto);
+console.log(conjunto.has(2));
+
+
+// WeakMap
+// Similar a map, pero solo acepta objetos como claves
+
+// WeakSet
+// Como Set, pero solo acepta objetos y no valores primitivos
+
+// Typed Arrays (ArrayBuffer)
+// Para trabajar con datos binarios en bruto
+
+
+```
+
 ## JavaScript III
 ```js
 /////////////////////
--- js/main.js
+++ js/main.js
@@ -1,186 +1,293 @@
-////////////////////
-// JavaScript IV //
+///////////////////
+// JavaScript V //
 
-// Introduccion a arrays y objetos. Metodos de strings y arrays
-// Arrays y objetos son estructuras de datos fundamentales
+// Objetos, clases y objetos globales. Almacenamiento persistente
+// Iteracion en arrays, objetos y arrays de objetos
+
+// Object literal - Objeto literal
+// La manera mas comun de crear objetos en JavaScript
+let auto = {
+    marca: "Renault 12",
+    modelo: 12,
+    anio: 2021,
+
+    getInfo: function() {
+        // this hace referencia al objeto desde el cual se esta invocando el metodo
+        return `Este auto es un ${this.marca} del año ${this.anio}`;
+    }
+}
+
+console.log(auto.getInfo());
+
+
+// Clases en JavaScript
+// Las clases en JS son una forma de crear objetos y metodos. Proporcionan una estructura clara para organizar el codigo
+// Fueron introducidas en ES6 y simplifican la creacion de objetos con las funciones constructoras anteriores
+
+class Persona {
+    #universidad = "UTN";
+
+    constructor(nombre, edad, cuadro) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.cuadro = cuadro;
+    }
+
+    saludar() {
+        console.log(`Hola! Soy ${this.nombre} tengo ${this.edad} y soy hincha de ${this.cuadro}`);
+    }
+}
 
 /*
-Uso principal:
-    Array: Lista ordenada de elementos
-    Objeto: Coleccion de pares clave-valor
+let moreNombre = prompt("Introduzca nombre");
+let moreEdad = parseInt(prompt("Introduzca edad"));
+let moreCuadro = prompt("Introduzca cuadro");
 
-Acceso a datos:
-    Array: Por indice -> 0
-    Objeto: Por clave
+let more = new Persona(moreNombre, moreEdad, moreCuadro);
+more.saludar();
+*/
 
-Metodos:
-    Array: .push(), .pop(), .map(). .forEach()
-    Objeto: Metodos personalizados
+let kevin = new Persona("Kevin", 23, "Boca Juniors");
+console.log(kevin);
+kevin.saludar();
 
-Iteracion:
-    Array: bucles, .forEach(), .map()
-    Objeto: for.. in, Object.keys(), Object.values()
+
+// Herencia
+// Creamos nuevas clases basadas en clases existentes
+// La clase hija puede heredar propiedades y metodos de la clase utilizando la palabra clave extends
+class Animal {
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
+
+    hacerSonido() {
+        console.log(`${this.nombre} hace un sonido`);
+    }
+}
+
+class Perro extends Animal {
+    ladrar() {
+        console.log(`${this.nombre} ladra`);
+    }
+}
+
+let miPerro = new Perro("Firulais");
+miPerro.hacerSonido();
+miPerro.ladrar();
+
+
+
+/////////////////////////////////////
+// Objetos globales en JavaScript //
+
+/** Objetos globales en JavaScript
+ * Son objetos que están disponibles en todo entorno de ejecución sin necesidad de importarlos o declararlos explícitamente
+ * 
+ * Objetos globales en el navegador
+ * window
+ */
+
+// alert(), prompt() y confirm() son metodos de window
+
+// setTimeout() setInterval()
+// Metodos para programar la ejecucion de codigo despues de un tiempo setTimeout o en intervalos regulares setInterval
+setTimeout(() => console.log("Hola despues de dos segundos"), 500)
+
+// location
+// Proporcionar informacion sobre la URL actual de la pagina y permite redireccionar a otras URL
+console.log(window.location.href);
+
+// navigator
+// Contiene informacion sobre el navegador, como la version, el agente de usuario y la geolocalizacion
+console.log(navigator.userAgent);
+
+// history
+// Proporcionar acceso al historial de navegacion del navegador
+
+
+//////////////
+// console //
+// Es un objeto global que proporciona metodos para acceder a la consola del navegador y se utiliza principalmente para depurar codigo y mostrar mensajes informativos, advertencias o errores
+
+console.log("Mensaje informativo"); // Muestra mensajes informativos en consola
+
+console.error("Nada de esto fue un error"); // Muestra errores por consola
+
+console.warn("Advertencia"); // Muestra advertencias
+
+
+let usuariosMuestra = [
+    { nombre: "Nahuel", edad: 30 },
+    { nombre: "Lautaro", edad: 25 }
+];
+
+console.table(usuariosMuestra) // Mostrar datos en forma de tabla, util para arrays y objetos 
+
+// console.time() y console.timeEnd()
+// Nos permite medir el tiempo de ejecucion de un bloque de codigo
+
+console.time("tiempo ejecucion");
+
+for(let i = 0; i < 1000; i++) {
+    // Simulacion de tarea
+}
+
+console.timeEnd("tiempo ejecucion");
+
+
+
+// Almacenamiento en JavaScript
+// Otros objetos globales del navegador: localStorage y sessionStorage
+// sessionStorage() localStorage() y Cookies
+// Son mecanismos del navegador para almacenar datos del lado del cliente
+
+
+// Permiten almacenar datos de manera persistente (localStorage) o temporal (sessionStorage)
+/*   ///////////////////
+    // localStorage //
+
+    // Descripcion //
+
+- Nos permite almacenar datos de manera persistente en el navegador.
+
+- Los datos almacenados en localStorage NO tienen una fecha de expiracion, esto significa que estaran disponilbes incluso despues de cerrar el navegador o apagar la computadora.
+
+
+    // Caracteristicas //
+- Capacidad de almacenamiento de 5-10 MB
+
+- Persistencia: Los datos permanecen almacenados hasta que son eliminados manualmente
+
+- Datos almacenados como strings: Todos los datos almacenados en localStorage son de tipo string. Si se quiere almacenar otros tipos de datos, como objetos, deben ser convertidos a strings (generalmente JSON)
+
+
+    // Metodos //
+- Guardar datos: localStorage.setItem(clave, valor);
+- Leer datos: localStorage.getItem(clave);
+- Eliminar un dato: localStorage.removeItem(clave);
+- Eliminar todos los datos: localStorage.clear();
+*/
+
+// localStorage()
+// Guarda datos que persisten incluso al cerrar el navegador y apagar la compu
+// Almacenar configuraciones de usuario, temas, carrito de compras, etc
+// Guardar datos en localStorage
+localStorage.setItem("nombre", "Matias");
+localStorage.setItem("tema", "oscuro");
+
+// Obtener datos del localStorage
+console.log(localStorage.getItem("nombre"));
+
+// Eliminamos un dato especifico
+localStorage.removeItem("tema");
+
+// Limpiamos todo el localStorage
+localStorage.clear();
+
+localStorage.setItem("carrito", JSON.stringify([
+    { id: 1, nombre: "Empanada", cantidad:2 }
+]));
+
+
+// sessionStorage()
+// Igual que localStorage pero la informacion dura lo que este abierta la pestaña (cambia la persistencia)
+/*
+    // Metodos //
+- Guardar datos: sessionStorage.setItem(clave, valor);
+- Leer datos: sessionStorage.getItem(clave);
+- Eliminar un dato: sessionStorage.removeItem(clave);
+- Eliminar todos los datos: sessionStorage.clear();
+*/
+
+/*   //////////////
+    // Cookies //
+
+    Autenticacion (sesion), preferencias del usuario, seguimiento de actividad
+
+    // Descripcion //
+
+- Las cookies son pequeños fragmentos de informacion que se almacenan en el navegador y se envian con cada peticion HTTP al servidor.
+
+- Mas antiguas que localStorage y sessionStorage y fueron ampliamente usadas, mantener la session del usuario, guardar preferencias etc
+
+
+    // Caracteristicas //
+- Capacidad de almacenamiento de 4KB por cookie.
+
+- Persistencia: Pueden tener una fecha de expiracion especifica. Si no se establece una fecha de expiracion, la cookie sera eliminada al cerrar la sesion del navegador.
+
+- Envío al servidor: A diferencia de localStorage y sessionStorage, las cookies se envian automaticamente al servidor con cada solicitud HTTP, lo que puede ser util pero tambien puede generar sobrecarga en la red
+
+- Almacenamiento por origen (dominio y protocolo): Los datos se almacenan por dominio, lo que significa que SOLO son accesibles dentro del mismo dominio
+*/
+
+// Crear una cookie
+document.cookie = "usuario=Alejandro; path=/"; // Se elimina al cerrar el navegador
+
+document.cookie = "pais=Argentina; expires=Fri, 31 Dec 2025 23:59:59; path=/;"; // La cookie se elimina el 31 de diciembre
+
+// Eliminamos cookies poniendo una fecha de expiracion en el pasado
+document.cookie = "usuario=; expires=Thu, 01 Jan 1970 00:00:00; path=/";
+
+
+
+// Almacenamiento de datos en JavaScript
+// Variables simples:   Valores unicos como numeros o cadenas
+// Objetos:             Representamos datos complejos con propiedades
+// Arrays:              Almacenar una lista de elementos, idealmente del mismo tipo
+// Arrays de objetos:   Util para manejar listas de elmentos complejos que contienen multiples propiedades
+
+/* Casos de uso
+    -  Objeto simple: Si solo tenemos una entidad como configuracion de usuario o un unico elemento que contiene datos con varias propiedades
+
+    - Array simple: Para una lista ordenada de elmentos individuales
+
+    - Array de objetos: Para listas de entidades complejas, cada una con sus propiedades
 */
 
 
-/////////////
-// Arrays //
-// Arrays para almacenar una lista ORDENADA de elementos
-let frutas = ["pera", "banana", "mandarina"];
 
-console.log(frutas[0]); // pera
-
-
-/////////////
-// Objetos//
-// Objetos para almacenar datos con propiedades CLAVE-VALOR
-let alumno = {
-    nombre: 'Camilo',
-    edad: 19,
-    ciudad: "Lanus",
-
-    agitar: function() {
-        console.log("Aguante Lanus");
-    }
+// Ejemplo practico
+// usuario es un objeto que representa a un usuario especifico, con datos personales y carrito de compras
+let usuario = { 
+    id: 1,
+    nombre: "Johnny Melavo",
+    email: "johnny@melavo.com",
+    
+    // carrito dentro de usuario es un array de objetos, donde cada objeto representa un producto en carrito
+    carrito: [
+        { productoId: 101, nombre: "Laptop", precio: 100000, cantidad: 1 },
+        { productoId: 201, nombre: "Teclado", precio: 10000, cantidad: 2 }
+    ]
 }
 
-// Notacion de punto
-console.log(alumno.nombre); //
+// inventario es un array de objetos que representa todos los productos disponibles en la tienda
+let inventario = [
+    { id: 101, nombre: "Laptop", precio: 100000, stock: 15 },
+    { id: 201, nombre: "Teclado", precio: 10000, stock: 20 }
+];
 
-// Notacion de corchete
-console.log(alumno["ciudad"]); //
+// La idea del array de objetos es almacenar de forma numerada, multiples objetos donde cada objeto tiene la misma estructura
+let listaChantapufis = [
+    { nombre: "Brad Pitt", edad: 17, ocupacion: "Actor" },
+    { nombre: "Angelina", edad: 60, ocupacion: "Super estrella hollywoodiense" },
+    { nombre: "El tio del cuento del tio", edad: 100, ocupacion: "Sospechosa" }
+];
 
-alumno.agitar();
-
-// Agregar una propiedad
-alumno.pais = "Argentina";
-
-console.log(alumno);
-
-// Eliminar una propiedad
-delete alumno.edad;
-console.log(alumno);
-
-
-// Ejemplo completo con un Objeto
-let libro = {
-    titulo: "El Eternauta",
-    autor: "Héctor Germán Oesterheld",
-    anio: 1957,
-
-    detalles: function() {
-        return `${this.titulo} es un libro escrito por ${this.autor} en el año ${this.anio}.`;
-    }
-};
-
-console.log(libro.detalles());
-
-libro.editorial = "Hora Cero Semanal";
-
-console.log(libro);
-
-
-////////////////////////////////////////////////
-// Object wrappers - Envolvedores de objetos //
-
-// JavaScript otorga metodos a tipos de datos primitivos
-// Por eso se dice que en JavaScript son todo objetos (incluso los tipos de datos que no son objetos)
-
-// Metodos de Strings en JavaScript
-// https://drive.google.com/drive/u/1/folders/1QJtNkPRSPVN4S2WCuwoeNcxrZbZkPH0y
-// https://www.w3schools.com/js/js_string_methods.asp
-
-
-// Convertir a mayusculas o minusculas
-let texto1 = "JavaScript esta re bueno";
-console.log(texto1.toUpperCase()); // JAVASCRIPT ESTA RE BUENO
-console.log(texto1.toLowerCase()); // javascript esta re bueno
-
-
-// Reemplazamos valores
-let texto2 = "JavaScript es dificil";
-let nuevoTexto2 = texto2.replace("dificil", "facil");
-console.log(nuevoTexto2);
-
-
-// Extaer una parte del string sin alterar el string original
-let texto3 = "Aprendiendo JavaScript";
-let parte3 = texto3.slice(11, 22);
-console.log(parte3);
-
-
-// Dividir un string en un array de subcadenas
-let texto4 = "Manzana, Naranja, Pomelo";
-let frutas4 = texto4.split(", ");
-console.log(frutas4);
-
-let pruebaLoop = "Alejandro";
-for(let i = 0; i < pruebaLoop.length; i++) {
-    console.log(pruebaLoop[i]);
+// iteracion de array de objetos con for clasico
+for(let i = 0; i < listaChantapufis.length; i++) {
+    console.log(listaChantapufis[i].ocupacion);
 }
 
+// forEach() 
+listaChantapufis.forEach(chantapufi => {
+    console.log(`Nombre: ${chantapufi.nombre}, ocupacion: ${chantapufi.ocupacion}`);
+});
 
-// Eliminamos los espacios en blanco, al principio y al final de un string (también teemos trimStart y trimEnd)
-let texto5 = "    JavaScript     ";
-console.log(texto5.trim());
-
-
-// Reemplazar un caracter por otro (recuerden que hay replace() y replaceAll())
-let texto6 = "Java    Script";
-console.log(texto6.replaceAll(" ", "")); // Con replaceAll reemplazamos TODOS
-
-
-// Devuelve el caracter en una posicion especifica
-let texto7= "Hola";
-console.log(texto7.charAt(0));
+// filter() para selecionar subconjunto
+let chantapufisAdultos = listaChantapufis.filter(chanta => chanta.edad > 18);
+console.log(chantapufisAdultos)
 
 
-// Verificamos si un string contiene un valor especifico
-let texto8 = "Aprendiendo JavaScript";
-console.log(texto8.includes("Java"));
-
-
-/*Ejercicio propuesto
-Crear una funcion que reciba una cadena de texto y
-    - La convierta a mayusculas
-    - Reemplace una palabra dentro del string
-    - Elimine los espacios en blanco sobrantes*/
-
-
-
-// Metodos de arrays en JavaScript
-// https://drive.google.com/drive/u/1/folders/1QJtNkPRSPVN4S2WCuwoeNcxrZbZkPH0y
-// https://www.w3schools.com/js/js_array_methods.asp
-
-// Un array es una estructura ORDENADA de datos que almacena varios valores en una sola variable
-// Pueden contener distintos tipos de datos: numeros, strings, booleanos, etc
-
-let numeros = [1, 2, 3, 4, 5];
-
-
-// Modificamos elementos de un array
-numeros[2] = 10;
-console.log(numeros);
-
-let verduras = ["Rucula", "Lechuga", "Albahaca"];
-
-
-// Añadimos uno o mas elementos al FINAL del array
-verduras.push("Tomate");
-console.log(verduras);
-
-
-// Eliminar el ultimo valor de un array
-let ultimaVerdura = verduras.pop();
-console.log(ultimaVerdura);
-console.log(verduras);
-
-
-// Eliminar el PRIMER valor de un array
-verduras.shift();
-console.log(verduras);
-
-
-// Añadimos uno o mas elementos al INICIO del array
-verduras.unshift("Ajo");
-console.log(verduras);
+// TODO: map() modificar el array sin cambiar el original
+// TODO: reduce()
